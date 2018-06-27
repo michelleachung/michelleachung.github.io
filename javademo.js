0d5a2ca128fca30e3f5c063f8007e752241d236d
michelleachung <40634426+michelleachung@users.noreply.github.com>
2018-06-27 09:53:32 -0400

    Add files via upload

---
+++ javademo.js
@@ -0,0 +1,15 @@
+function welcome() {
+  window.open("htmldemo.html")
+}
+
+function instagram() {
+  window.open("https://www.instagram.com/michelleachung/?hl=en"); 
+}
+// function welcometowebsite (){
+//   onclick="this.innerHTML= 'Webpage opened'">Click to see webpage.
+// }
+
+
+// .button {
+//   onclick="this.innerHTML= 'Webpage opened'">Click to see webpage.</p>
+// }
---
+++ website/javademo.js
@@ -0,0 +1,12 @@
+function welcome() {
+  window.open("htmldemo.html")
+}
+
+// function welcometowebsite (){
+//   onclick="this.innerHTML= 'Webpage opened'">Click to see webpage.
+// }
+
+
+// .button {
+//   onclick="this.innerHTML= 'Webpage opened'">Click to see webpage.</p>
+// }
